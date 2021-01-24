7a94b8187a9253e78d041028b5370f898291bb20
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-01-24 20:55:53 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## CLONNING and RUNNING
 
 ```sh
-curl -sSL https://raw.githubusercontent.com/YurinDoctrine/arch-linux-base-setup/main/arch-linux-base-setup.sh >arch-linux-base-setup.sh &&
+curl -fsSL https://raw.githubusercontent.com/YurinDoctrine/arch-linux-base-setup/main/arch-linux-base-setup.sh >arch-linux-base-setup.sh &&
  chmod 755 arch-linux-base-setup.sh &&
  ./arch-linux-base-setup.sh
 ```
--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -254,9 +254,14 @@ echo -e "
 "
 
 extra() {
-    curl https://raw.githubusercontent.com/YurinDoctrine/ultra-gaming-setup-wizard/main/ultra-gaming-setup-wizard.sh >ultra-gaming-setup-wizard.sh &&
+    curl -fsSL https://raw.githubusercontent.com/YurinDoctrine/ultra-gaming-setup-wizard/main/ultra-gaming-setup-wizard.sh >ultra-gaming-setup-wizard.sh &&
         chmod 755 ultra-gaming-setup-wizard.sh &&
         ./ultra-gaming-setup-wizard.sh
+}
+extra2() {
+    curl -fsSL https://raw.githubusercontent.com/YurinDoctrine/secure-linux/master/secure.sh >secure.sh &&
+        chmod 755 secure.sh &&
+        ./secure.sh
 }
 
 final() {
