cbaf628885e3abe45de4cec0e53b9a8a93b482c1
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-02-09 15:30:25 +0300

    Add files via upload

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -10,10 +10,9 @@ sudo pacman -Sy &&
 # ------------------------------------------------------------------------
 
 # Setting up locales
-echo -e "Setup language to en_GB and set locale"
 sudo sed -i 's/^#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/' /etc/locale.gen
 sudo locale-gen
-localectl set-locale LANG="en_GB.UTF-8" LC_TIME="en_GB.UTF-8"
+localectl set-locale LANG=en_GB.UTF-8 LC_TIME=en_GB.UTF-8
 
 # ------------------------------------------------------------------------
 
