8215277a6ac33a54fd64f77eccd2385cafc7a9ae
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-02-28 00:37:29 +0300

    Add files via upload

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -58,7 +58,6 @@ PKGS=(
     'xscreensaver'        # A screen saver and locker for the X
     'xfce4-goodies'       # Enhancements for the Xfce4
     'xfce4-power-manager' # Power Manager
-    'lxqt-notificationd'  # Notification indicator
     'dmenu'               # Generic menu for X
     'gmrun'               # A lightweight application launcher
     'gsimplecal'          # A simple, lightweight calendar
@@ -314,6 +313,7 @@ cd /tmp &&
 sudo cp -R cbpp-lxdm-theme/cbpp-lxdm-theme/data/etc/lxdm/* /etc/lxdm
 sudo cp -R cbpp-lxdm-theme/cbpp-lxdm-theme/data/usr/share/lxdm/themes/* /usr/share/lxdm/themes
 cd
+echo -e "XDG_CURRENT_DESKTOP=Unity" | sudo tee -a /etc/environment
 
 # ------------------------------------------------------------------------
 
