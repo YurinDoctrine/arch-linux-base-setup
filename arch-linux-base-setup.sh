04399f2bd5834ffc5997ab4a1dbbba474ea90a09
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-05-10 20:17:39 +0300

    Add files via upload

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -82,6 +82,7 @@ PKGS=(
     'udiskie'             # An udisks2 front-end written in python
     'tint2'               # A simple, unobtrusive and light panel for Xorg
     'lxappearance'        # Set System Themes
+    #'lxdm'               # A lightweight display manager
     'lxsession'           # LXDE PolicyKit authentication agent
     'xfce4-notifyd'       # Notification Daemon
 
