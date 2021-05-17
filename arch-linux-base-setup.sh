1d896d33b930fa1efc381a7afa9d16dbac09afec
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-05-17 08:48:00 +0000

    Add files via upload

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Before hop in
 sudo pacman -Syy &&
-    sudo pacman -S --needed --noconfirm base-devel pacman-contrib systemd git go &&
+    sudo pacman -S --needed --noconfirm base-devel pacman-contrib psmisc systemd git go &&
     sudo pacman -S --needed --noconfirm yay
 
 # ------------------------------------------------------------------------
