8e89bb5641aa2892e545e0654a104c9b2aa7a8fa
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-05-28 12:16:06 +0300

    Add files via upload

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -166,7 +166,7 @@ sudo sed -i -e 's|#HandleLidSwitch=suspend|HandleLidSwitch=suspend|g' /etc/syste
 # ------------------------------------------------------------------------
 
 echo "Disabling buggy cursor inheritance"
-# When you boot with multiple monitors the cursor can look huge. This fixes this...
+# When you boot with multiple monitors the cursor can look huge. This fixes that...
 echo -e "[Icon Theme]
 #Inherits=Theme
 " | sudo tee /usr/share/icons/default/index.theme
