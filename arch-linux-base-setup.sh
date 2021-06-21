cfb8ed8c050406a74113caa44a4b4bf32091d774
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-06-22 00:52:57 +0300

    Edit arch-linux-base-setup.sh

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Before hop in
 sudo pacman -Syy &&
-    sudo pacman -S --needed --noconfirm 9base curl kitty networkmanager pacman-contrib procps-ng psmisc reflector systemd git go xorg-server &&
+    sudo pacman -S --needed --noconfirm 9base curl git networkmanager pacman-contrib procps-ng psmisc reflector systemd xorg-server &&
     sudo pacman -S --needed --noconfirm yay
 
 # ------------------------------------------------------------------------
@@ -55,35 +55,7 @@ echo -e "Installing Base System"
 PKGS=(
     # --- Importants
 
-    'arandr'               # Provide a simple visual front end for XRandR
-    'engrampa'             # Archive manipulator for MATE
-    'mate-power-manager'   # MATE Power Manager
-    'mksh'                 # MirBSD Korn Shell
-    'dmenu'                # A generic menu for X
-    'gmrun'                # A lightweight application launcher
-    'gsimplecal'           # A simple, lightweight calendar
-    'conky'                # A system monitor software for the X Window System
-    'dunst'                # Customizable and lightweight notification-daemon
-    'featherpad'           # Lightweight Qt plain text editor
-    'openbox'              # A lightweight, powerful, and highly configurable stacking window manager
-    'scrot'                # Simple command-line screenshot utility
-    'slock'                # A simple screen locker for X
-    'udiskie'              # An udisks2 front-end written in python
-    'pcmanfm-qt'           # The LXQt file manager
-    'ranger'               # A file manager with vi key bindings written in python but with an interface that rocks
-    'simplescreenrecorder' # A feature-rich screen recorder that supports X11 and OpenGL
-    'tint2'                # A simple, unobtrusive and light panel for Xorg
-    'xwallpaper'           # A lightweight and simple desktop background setter for X Window
-    'xcompmgr'             # A simple composite manager
-    'lxappearance'         # Set System Themes
-    'lxsession'            # LXDE PolicyKit authentication agent
-    #'lxdm'                 # A lightweight display manager
-
-    # --- Network
-
-    'filezilla'        # FTP Client
-    'irssi'            # Terminal based IRC
-    'transmission-gtk' # BitTorrent client
+    'mksh' # MirBSD Korn Shell
 
     # GENERAL UTILITIES ---------------------------------------------------
 
@@ -95,41 +67,6 @@ PKGS=(
 
     'ccache' # Compiler cacher
     'meson'  # Build system that use python as a front-end language and Ninja as a building backend
-
-    # --- Audio
-
-    'alsaplayer' # A heavily multi-threaded PCM player
-    'pasystray'  # PulseAudio system tray
-    'playerctl'  # Utility to control media players via MPRIS
-    'pulsemixer' # CLI and curses mixer for PulseAudio
-
-    # --- Bluetooth
-
-    'blueman' # GTK+ Bluetooth Manager
-
-    # TERMINAL UTILITIES --------------------------------------------------
-
-    'fish'   # The friendly interactive shell
-    'htop'   # Interactive process viewer
-    'neovim' # Fork of Vim aiming to improve user experience, plugins, and GUIs
-
-    # DISK UTILITIES ------------------------------------------------------
-
-    'gparted' # Disk utility
-
-    # GRAPHICS, VIDEO AND DESIGN ------------------------------------------
-
-    'pinta'    # A simplified alternative to GIMP
-    'viewnior' # A simple, fast and elegant image viewer
-    'vlc'      # A free and open source cross-platform multimedia player and framework
-
-    # PRINTING ------------------------------------------------------------
-
-    'abiword'               # Fully-featured word processor
-    'atril'                 # PDF viewer
-    'cups'                  # The CUPS Printing System - daemon package
-    'gnumeric'              # A powerful spreadsheet application
-    'system-config-printer' # A CUPS printer configuration tool and status applet
 
 )
 
@@ -188,16 +125,6 @@ sudo killall -HUP pulseaudio
 # Prevent stupid error beeps*
 sudo rmmod pcspkr
 echo -e "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
-
-# ------------------------------------------------------------------------
-
-echo -e "Disable bluez daemon(opt-out)"
-sudo systemctl disable --now bluetooth.service
-
-# ------------------------------------------------------------------------
-
-echo -e "Disable cups daemon(opt-out)"
-sudo systemctl disable --now cups.service
 
 # ------------------------------------------------------------------------
 
@@ -270,70 +197,6 @@ find /usr/share/doc/ -empty | xargs sudo rmdir || true
 sudo rm -rfd /usr/share/groff/* /usr/share/info/* /usr/share/lintian/* \
     /usr/share/linda/* /var/cache/man/* /usr/share/man/*
 sync
-
-# ------------------------------------------------------------------------
-
-# Implement .config/ files of the openbox
-cd /tmp &&
-    sudo rm -rfd /usr/share/icons/CBPP
-git clone --branch 11 https://github.com/CBPP/cbpp-icon-theme.git &&
-    sudo cp -R cbpp-icon-theme/cbpp-icon-theme/data/usr/share/icons/* /usr/share/icons &&
-    sudo rm -rfd /usr/share/themes/CBPP
-git clone --branch 11 https://github.com/CBPP/cbpp-ui-theme.git &&
-    sudo cp -R cbpp-ui-theme/cbpp-ui-theme/data/usr/share/themes/* /usr/share/themes &&
-    sudo mkdir /usr/share/backgrounds
-sudo rm -rfd /usr/share/backgrounds/*
-git clone --branch 11 https://github.com/CBPP/cbpp-wallpapers.git &&
-    sudo cp -R cbpp-wallpapers/cbpp-wallpapers/data/usr/share/backgrounds/* /usr/share/backgrounds &&
-    git clone --branch 11 https://github.com/CBPP/cbpp-lxdm-theme.git &&
-    sudo rm -rfd /usr/share/lxdm/themes/*
-sudo cp -R cbpp-lxdm-theme/cbpp-lxdm-theme/data/etc/lxdm/* /etc/lxdm
-sudo cp -R cbpp-lxdm-theme/cbpp-lxdm-theme/data/usr/share/lxdm/themes/* /usr/share/lxdm/themes
-sudo rm -rfd /home/$USER/.config/*
-sudo rm -rfd /etc/skel/.config/*
-git clone https://github.com/YurinDoctrine/.config.git &&
-    sudo cp -R .config/.conkyrc $HOME
-sudo cp -R .config/.gmrunrc $HOME
-sudo cp -R .config/.gtkrc-2.0 $HOME
-sudo cp -R .config/.gtkrc-2.0.mine $HOME
-sudo cp -R .config/.fonts.conf $HOME
-sudo cp -R .config/.gtk-bookmarks $HOME
-sudo cp -R .config/* $HOME/.config
-sudo cp -R .config/.conkyrc /etc/skel
-sudo cp -R .config/.gmrunrc /etc/skel
-sudo cp -R .config/.gtkrc-2.0 /etc/skel
-sudo cp -R .config/.gtkrc-2.0.mine /etc/skel
-sudo cp -R .config/.fonts.conf /etc/skel
-sudo cp -R .config/.gtk-bookmarks /etc/skel
-sudo cp -R .config/.conkyrc /root
-sudo cp -R .config/.gmrunrc /root
-sudo cp -R .config/.gtkrc-2.0 /root
-sudo cp -R .config/.gtkrc-2.0.mine /root
-sudo cp -R .config/.fonts.conf /root
-sudo cp -R .config/.gtk-bookmarks /root
-sudo mkdir /etc/skel/.config
-sudo cp -R .config/* /etc/skel/.config
-sudo mkdir /root/.config
-sudo cp -R .config/* /root/.config
-sudo mv /etc/skel/.config/conkywonky /usr/bin
-sudo mv /etc/skel/.config/tint2restart /usr/bin
-sudo mv /etc/skel/.config/cbpp-exit /usr/bin
-sudo mv /etc/skel/.config/cbpp-gksudo /usr/bin
-sudo mv /etc/skel/.config/cbpp-help-pipemenu /usr/bin
-sudo mv /etc/skel/.config/cbpp-compositor /usr/bin
-sudo mv /etc/skel/.config/cbpp-include.cfg /usr/bin
-sudo mv /etc/skel/.config/cbpp-places-pipemenu /usr/bin
-sudo mv /etc/skel/.config/cbpp-recent-files-pipemenu /usr/bin
-sudo mv /etc/skel/.config/cbpp-welcome /usr/bin
-sudo find /home/$USER/.config/ | egrep '\cbpp-' | xargs sudo rm -f
-sudo find /root/.config/ | egrep '\cbpp-' | xargs sudo rm -f
-sudo find /home/$USER/.config/ | egrep '\conkywonky' | xargs sudo rm -f
-sudo find /root/.config/ | egrep '\conkywonky' | xargs sudo rm -f
-sudo find /home/$USER/.config/ | egrep '\tint2restart' | xargs sudo rm -f
-sudo find /root/.config/ | egrep '\tint2restart' | xargs sudo rm -f
-
-echo -e "export XDG_CURRENT_DESKTOP=LXDE
-export QT_QPA_PLATFORMTHEME=gtk2" | sudo tee -a /etc/environment
 
 # ------------------------------------------------------------------------
 
