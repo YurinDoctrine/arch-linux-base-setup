77a1b68a995c7826bbe1478ec2a5d94df1c15150
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-06-23 22:52:51 +0300

    Delete 'fakeroot' package

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syy &&
     sudo pacman -S --needed --noconfirm reflector
 sudo pacman -S --needed --noconfirm yay
-sudo pacman -S --needed --noconfirm 9base curl fakeroot git networkmanager pacman-contrib procps-ng psmisc systemd xorg-server
+sudo pacman -S --needed --noconfirm 9base curl git networkmanager pacman-contrib procps-ng psmisc systemd xorg-server
 
 # ------------------------------------------------------------------------
 
