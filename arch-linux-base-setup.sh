2ca38df35079f3e6f31b977c3fde1a83b624ff2c
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-08-31 12:49:58 +0300

    Edit arch-linux-base-setup.sh

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -9,6 +9,7 @@ sudo pacman -S --needed --noconfirm yay
 
 # Setting up locales & timezones
 echo -e "LANG=en_GB.UTF8" | sudo tee -a /etc/environment
+echo -e "LANGUAGE=en_GB.UTF8" | sudo tee -a /etc/environment
 echo -e "LC_ALL=en_GB.UTF8" | sudo tee -a /etc/environment
 sudo sed -i -e 's/^#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/' /etc/locale.gen
 sudo locale-gen en_GB.UTF-8
