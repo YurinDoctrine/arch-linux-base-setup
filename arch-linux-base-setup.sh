a7307db8b6df9d878f95bd876bfadbed4c2366d2
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-10-05 09:27:09 +0300

    edit arch-linux-base-setup.sh

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -182,7 +182,11 @@ sudo sed -i -e '/^\/\/sr/d' /etc/fstab
 # ------------------------------------------------------------------------
 
 ## Set some ulimits to unlimited
-echo -e "* soft nproc unlimited
+echo -e "* soft memlock unlimited
+* hard memlock unlimited
+root soft memlock unlimited
+root hard memlock unlimited
+* soft nproc unlimited
 * hard nproc unlimited
 root soft nproc unlimited
 root hard nproc unlimited
