c0756c3ab3a2d4e8946f2f461e417e9183f63e12
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-05 15:56:21 +0300

    edit arch-linux-base-setup.sh

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -362,6 +362,7 @@ find /usr/share/doc/ | egrep '\.tex' | xargs sudo rm -f
 find /usr/share/doc/ -empty | xargs sudo rmdir || true
 sudo rm -rfd /usr/share/groff/* /usr/share/info/* /usr/share/lintian/* \
 /usr/share/linda/* /var/cache/man/* /usr/share/man/*
+yay -Rcc man-pages
 
 # ------------------------------------------------------------------------
 
