eaf38ea8a1323762926cb98484f13a6e426f2efe
Yurin Doctrine <doctriney@protonmail.com>
2022-12-03 12:10:02 +0300

    edit arch-linux-base-setup.sh

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -121,7 +121,8 @@ gsettings set org.gnome.desktop.background color-shading-type vertical
 # ------------------------------------------------------------------------
 
 # Set environment variables
-echo -e "LD_DEBUG_OUTPUT=0
+echo -e "LD_TRACE_LOADED_OBJECTS=0
+LD_DEBUG_OUTPUT=0
 LD_BIND_NOW=1
 RTLD_LAZY=1
 KDE_NOUNLOAD=1
