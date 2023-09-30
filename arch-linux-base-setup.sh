428b8e4c5b7c6111fc3db36ee1ae5303062977e4
Yurin Doctrine <doctriney@protonmail.com>
2023-09-30 10:42:13 +0300

    edit arch-linux-base-setup.sh

--- arch-linux-base-setup.sh
+++ arch-linux-base-setup.sh
@@ -142,7 +142,7 @@ kwriteconfig5 --file kwalletrc --group Wallet --key "First Use" --type bool fals
 # ------------------------------------------------------------------------
 
 # Set environment variables
-echo -e "LD_PRELOAD=/usr/lib/libjemalloc.so
+echo -e "LD_PRELOAD=/usr/lib/libjemalloc.so.2
 MALLOC_CHECK=0
 MALLOC_TRACE=0
 MESA_DEBUG=0
